47b93667f1580878ba2bdc12623671285ba976b3
lmillan9709 <73262977+lmillan9709@users.noreply.github.com>
2020-11-18 17:56:10 -0500

    Add files via upload

--- archivo.js
+++ archivo.js
@@ -1,1 +1,11 @@
-alert("Bienvenido a mi pagina del Universo")
+
+
+
+function mostrar() {
+var nombre=document.getElementById('txtnombre').value;
+var celular=document.getElementById('txtcelular').value;
+var correo=document.getElementById('txtcorreo').value;
+alert("Se recibio el mensaje del señor " +nombre+ "numero de celular" +celular+ "y correo " +correo+ " Deja el siguiente comentario :" +comentario)
+console.log("Se recibio el mensaje del señor "+nombre+" con numero de celular "+celular+ " y correo "+correo+" Deja el siguiente comentario : "+comentario)
+document.getElementById('respuesta').innerHTML="Se recibio el mensaje del señor "+nombre+" con numero de celular "+celular+ " y correo "+correo+" . Deja el siguiente comentario : "+comentario
+}
